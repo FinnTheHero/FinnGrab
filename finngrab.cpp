f0791954b17f880f78c7b0c46a596fe949d34a7f
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2023-08-17 10:36:01 +0400

    Update 0.2
    Split up code into functions for better usability
    Added main display loop
    as well as Array that holds the function pointers
    Added Windows 10 logo that is printed by line-by-line method in main for loop

--- finngrab.cpp
+++ finngrab.cpp
@@ -1,23 +1,37 @@
+// FinnGrab will display information as well as logo using line-by-line structure
+
 #include <iostream>
 #include <Windows.h>
+#include <vector>
 
-void upTime ();
-void ram ();
-void screenResolution ();
-void finnGrabVersion ();
+std::vector<std::string> windows10Logo () {
+    std::vector<std::string> artArray = {
+        "                                    ..       ",
+        "                             ..oodMMMM       ",
+        "                    ..oodMMMMMMMMMMMMM       ",
+        "        ...oodMMM  MMMMMMMMMMMMMMMMMMM       ",
+        "   oooMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ",
+        "   MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ",
+        "   MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ",
+        "   MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ",
+        "   MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ",
+        "   MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ",
+        "                                             ",
+        "   MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ",
+        "   MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ",
+        "   MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ",
+        "   MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ",
+        "   MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ",
+        "   ^^^MMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM       ",
+        "          ```^^^^  MMMMMMMMMMMMMMMMMMM       ",
+        "                        ```^^^^^^MMMMM       "
+    };
 
-int main() {
-    
-    finnGrabVersion();
-    upTime();
-    screenResolution();
-    ram();
-
-    return 0;
+    return artArray;
 }
 
 void finnGrabVersion () {
-    std::cout << "FinnGrab Version: 0.1" << std::endl;
+    std::cout << "FinnGrab Version: 0.2" << std::endl;
 }
 
 void upTime () {
@@ -52,4 +66,48 @@ void screenResolution () {
     int screenHeight = GetSystemMetrics(SM_CYSCREEN);
 
     std::cout << "Resolution: " << screenWidth << " x " << screenHeight << std::endl;
+}
+
+int main () {
+    // Create an array of pointers to the functions
+    void (*functionPointers[])() = { finnGrabVersion, upTime, screenResolution, ram };
+
+    // Calculate number of elements in the array
+    int numFunctions = sizeof(functionPointers) / sizeof(functionPointers[0]);
+
+    // Get logo art
+    std::vector<std::string> artArray = windows10Logo();
+ 
+    // Calculate the number of rows in the array
+    std::size_t artArrayLength = artArray.size();    
+
+    // Create iterator for loop
+    int iterator = 0;
+   
+    // Find if art is larger or system information and make iterator based on that
+    if (artArrayLength > numFunctions) {
+        iterator = artArrayLength;
+    } else {
+        iterator = numFunctions;
+    }
+    
+    // Place holder when system information is more than logo
+    std::string placeHolder = "                                             ";
+    
+    
+    // Use loop to call each function as well as logo ASCII art
+    for (int i=0; i < iterator; i++) {
+        if (artArrayLength > i) {
+            std::cout << artArray[i];
+        } else {
+            std::cout << placeHolder;
+        }
+
+        if (numFunctions > i) {
+            functionPointers[i]();
+        } else {
+            std::cout << std::endl;
+        }
+    }
+    return 0;
 }
